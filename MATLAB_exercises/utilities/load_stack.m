08734b42c11580dd6303d3ce20f31d94622ca4a0
Rob Campbell <git@raacampbell.com>
2016-05-03 10:05:27 +0200

    flesh out comments a little

--- MATLAB_exercises/utilities/load_stack.m
+++ MATLAB_exercises/utilities/load_stack.m
@@ -1,9 +1,17 @@
-function tfStack = load_stack(strFilename)
-
-% tfStack = load_stack(strFilename)
+function tfStack = load_stack(fileName)
+% Load a TIFF stack
 %
-% 'strFileName' is a string containg the name of a '.tif' file to load. The
-% image stack will be returned in 'tfStack', which is a variable with
-% dimensions (x, y, frames).
+% tfStack = load_stack(fileName)
+%
+% Purpose
+% Loads a TIFF stack into MATLAB. Here, a TIFF stack is a series of TIFF
+% images that corresponds to a time-series of images. 
+%
+%
+% Inputs
+% 'fileName' - a string containing the name of a '.tif' file to load. The
+%              image stack will be returned in the output variable
+%             'tfStack', which has dimensions (x, y, frames).
 
-tfStack = double(TIFFStack(strFilename));
+
+tfStack = double(TIFFStack(fileName));
