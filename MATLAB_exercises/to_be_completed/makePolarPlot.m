33dbb4171b25d8d5f268e29c62220c88b0fdd418
Rob Campbell <git@raacampbell.com>
2016-05-03 11:58:26 +0200

    expand some of the help text

--- MATLAB_exercises/to_be_completed/makePolarPlot.m
+++ MATLAB_exercises/to_be_completed/makePolarPlot.m
@@ -14,9 +14,11 @@ vnPeakWindow =
 vfMeanResp = 
 
 % To make the polar plot, you need to know the orientation in radians for
-% each stimulus. Hint: 'pi' returns the value of pi; 'linspace' creates a
-% vector of equally spaced numbers between two values.
+% each stimulus (MATLAB thinks in radians not degrees). 
+% Hints: a) 'pi' returns the value of pi and there are 2*pi radians in 360 degrees. 
+%        b) 'linspace' creates a vector of equally spaced numbers between two values.
 vfStimOri = 
 
 % Create a polar plot using the command 'polar'
+% Note: both input variables to polar should have the same size (they should both be row vectors or both be column vectors)
 polar
--- MATLAB_exercises/to_be_completed/meanTraces.m
+++ MATLAB_exercises/to_be_completed/meanTraces.m
@@ -19,7 +19,7 @@ nNumStimuli =
 % How many trials were there for each stimulus?
 nNumTrials = 
 
-% How many total stimulus presentations were there?
+% How many total stimulus presentations were there (use variable names not numbers)?
 nTotalStimuli = 
 
 % What was the duration of the "blank" stimulus?
@@ -28,34 +28,51 @@ tBlankDuration =
 % What was the duration of the "real" stimulus?
 tStimDuration = 
 
-% What was the total duration of each stimulus presentation (blank + stim)?
+% What was the total duration of each stimulus presentation (blank + stim)? (use variable names not numbers)
 tTotalStimDuration = 
 
 % Estimate the frame rate of imaging. Hint: The function 'numel' returns
-% how many elements there are in a vector.
+% how many elements there are in a vector. (use variable names not numbers)
 tFrameRate = 
 
 % How many frames were there in each stimulus presentation?
+%(use variable names not numbers)
 nTotalStimFrames = 
 
-% Allocate the return variable 'mfResponse'. Hint: the function 'zeros'
-% creates a variable of specified dimensions.
+% Allocate the return variable 'mfResponse', which should be an empty matrix of zeros.
+% Each row will eventually contain the average data of all triels from one stimulus.
+% Each column will correspond to data from one time point with respect to the start of the stimulus
+% Hint: the function 'zeros' and think how many frames per stimulus and how many stimuli
+% creates a variable of specified dimensions. (use variable names not numbers)
 mfResponse = 
 
 % Loop over presentations
 for nPresentation = 1:nTotalStimuli
-   % - Create a window for frames in 'vfTrace_dFF0' corresponding to this
-   % stimulus presentation. Hint: (1:N) creates a vector of numbers
-   % counting 1 2 3 ... N
+
+   % ONE: 
+   % You first need to create a set of index values that will pull out the frames
+   % from 'vfTrace_dFF0' that correspond to this stimulus presentation. So the first
+   % pass through the loop this will be 1:nTotalStimFrames, the second pass through the loop
+   % it will 1+nTotalStimFrames:nTotalStimFrames*2  and so on
+   %  Hint: (1:N) creates a vector of numbers that goes 1 2 3 ... N
+   %  Remember that this will need to increment to the right value on each pass through the loop
    vnWindow = 
    
-   % - Which stimulus direction was presented? This information is in
-   % 'Orientation'. Convert it to an index into 'mfResponse'. Hint: The
-   % directions span 360 degrees, and there are 16 of them.
+   % TWO:
+   % Find the stimulus direction that was presented and assign this to fThisOri
+   % The stimulus direction is in the Orientation vector. 
+   % hint: You already know which indexes correspond to this orientation from vnWindow
    fThisOri = 
+
+   % THREE:
+   % Your variable mfResponse is a matrix in which each row comprises data from one stimulus. 
+   % we need to figure out into which which row data from this orientation will go. To do this,
+   % you now need to convert fThisOr (the orientation value) to an index that has a value between 1 and nNumStimuli 
+   % Hint: The directions span 360 degrees, and there are 16 of them. 
    nThisOriIndex = 
    
-   % - Add the frames from 'vfTrace_dFF0' into 'mfResponse'. You will need
+   % FOUR:
+   % - Add the dF/F data from 'vfTrace_dFF0' into the matrix 'mfResponse'. You will need
    % to use the stimulus index for 'mfResponse' and the window you made for
    % 'vfTrace_dFF0'. Hint: mfResponse(nIndex, :) assigns something to all
    % the entries along the dimension with the ':'.
