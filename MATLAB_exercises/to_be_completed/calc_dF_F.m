294aac7e6b9ab916367c7560148b7bf728dc76a6
Maxime RIO <maxime.rio@unibas.ch>
2017-05-11 18:13:52 +0200

    typo

--- MATLAB_exercises/to_be_completed/calc_dF_F.m
+++ MATLAB_exercises/to_be_completed/calc_dF_F.m
@@ -1,5 +1,5 @@
 function vfTrace_dFF0 = calc_dF_F(vfTrace)
 
-% 'vfTrace' is a vector of raw fluourescence values for each frame. Compute
+% 'vfTrace' is a vector of raw fluorescence values for each frame. Compute
 % the 'F0' value, which is the median of the trace. Then compute (F-F0)/F0,
 % and return the result in a variable 'vfTrace_dFF0'.
